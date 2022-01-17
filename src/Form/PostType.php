caa5a00f6e04ea48bb7fc976078137d3e6bef515
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2022-01-17 17:00:56 +0100

    first commit

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,6 +17,7 @@ class PostType extends AbstractType
             //->add('alias')
             ->add('content')
             ->add('photo')
+            ->add('submit', SubmitType::class)
         ;
     }
 
