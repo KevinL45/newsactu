ed93303930ff4023c0e9a29f8a4510ea16414df9
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2022-01-18 22:46:53 +0100

    first commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * ex : https://127.0.0.1:8000
      * @Route("/", name="default_home", methods={"GET"})
      */
-    public function home(){
-        //$posts = $this->entityManager->getRepository(Post::class);
-        return $this->render('default/home.html.twig'/*,[
+    public function home()
+    {
+        $posts = $this->entityManager->getRepository(Post::class);
+
+        return $this->render('default/home.html.twig',[
             'posts' => $posts
-        ]*/);
+        ]);
     }
 }
